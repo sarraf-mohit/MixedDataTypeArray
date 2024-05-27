87df375937361e7a9514ce960884e351c4d0783c
Mohit <Mohit.Sarraf@qualitestgroup.com>
2024-05-27 21:26:07 +0530

    Mixed Datatype array code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MixedTypeArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an array of type object to store mixed datatypes
+            object[] mixedArray = { 42, "Hello", 2.06f, 'A', true, 7.14 };
+
+            Console.WriteLine("Elements in the mixed array:");
+            foreach (var element in mixedArray) 
+            {
+                Console.WriteLine($"Type: {element.GetType()}, Value: {element}");
+            }
+        }
+    }
+}
